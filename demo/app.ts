96240b01116a33e0bdf02cb9cc40b68c4c579073
ismail-codar <icodar@hotmail.com>
2017-09-25 20:55:17 +0300

    Build configurations.

--- demo/app.ts
+++ demo/app.ts
@@ -1,8 +1,8 @@
 import { routerLinkCheck } from 'simplester/web';
 import { S } from 'surplus';
-import { RoutedApp } from 'simplester';
+import { Simplester } from 'simplester';
 
-const app = new RoutedApp(
+const app = new Simplester(
   {
     viewSelector: '#main',
     routeRenderer: (routeTreeItem, viewSelector, next) => {
@@ -17,62 +17,62 @@ const app = new RoutedApp(
     }
   },
   {
-    path: 'demo',
+    path: '/demo',
     render: require('./default'),
     childs: [
-      { path: 'button', render: require('./button') },
-      { path: 'card', render: require('./card') },
-      { path: 'checkbox', render: require('./checkbox') },
-      { path: 'dialog', render: require('./dialog') },
+      { path: '/button', render: require('./button') },
+      { path: '/card', render: require('./card') },
+      { path: '/checkbox', render: require('./checkbox') },
+      { path: '/dialog', render: require('./dialog') },
       {
-        path: 'drawer',
+        path: '/drawer',
         abstract: true,
         childs: [
           {
-            path: 'temporary-drawer',
+            path: '/temporary-drawer',
             render: require('./drawer/temporary-drawer')
           },
           {
-            path: 'persistent-drawer',
+            path: '/persistent-drawer',
             render: require('./drawer/persistent-drawer')
           },
           {
-            path: 'permanent-drawer-below-toolbar',
+            path: '/permanent-drawer-below-toolbar',
             render: require('./drawer/permanent-drawer-below-toolbar')
           },
           {
-            path: 'permanent-drawer-above-toolbar',
+            path: '/permanent-drawer-above-toolbar',
             render: require('./drawer/permanent-drawer-above-toolbar')
           }
         ]
       },
-      { path: 'elevation', render: require('./elevation') },
-      { path: 'fab', render: require('./fab') },
-      { path: 'grid-list', render: require('./grid-list') },
-      { path: 'icon-toggle', render: require('./icon-toggle') },
-      { path: 'layout-grid', render: require('./layout-grid') },
-      { path: 'linear-progress', render: require('./linear-progress') },
-      { path: 'list', render: require('./list') },
-      { path: 'radio', render: require('./radio') },
-      { path: 'ripple', render: require('./ripple') },
-      { path: 'select', render: require('./select') },
-      { path: 'simple-menu', render: require('./simple-menu') },
-      { path: 'slider', render: require('./slider') },
-      { path: 'snackbar', render: require('./snackbar') },
-      { path: 'switch', render: require('./switch') },
-      { path: 'tabs', render: require('./tabs') },
-      { path: 'textfield', render: require('./textfield') },
-      { path: 'theme', render: require('./theme') },
-      { path: 'toolbar', render: require('./toolbar') },
-      { path: 'typography', render: require('./typography') },
+      { path: '/elevation', render: require('./elevation') },
+      { path: '/fab', render: require('./fab') },
+      { path: '/grid-list', render: require('./grid-list') },
+      { path: '/icon-toggle', render: require('./icon-toggle') },
+      { path: '/layout-grid', render: require('./layout-grid') },
+      { path: '/linear-progress', render: require('./linear-progress') },
+      { path: '/list', render: require('./list') },
+      { path: '/radio', render: require('./radio') },
+      { path: '/ripple', render: require('./ripple') },
+      { path: '/select', render: require('./select') },
+      { path: '/simple-menu', render: require('./simple-menu') },
+      { path: '/slider', render: require('./slider') },
+      { path: '/snackbar', render: require('./snackbar') },
+      { path: '/switch', render: require('./switch') },
+      { path: '/tabs', render: require('./tabs') },
+      { path: '/textfield', render: require('./textfield') },
+      { path: '/theme', render: require('./theme') },
+      { path: '/toolbar', render: require('./toolbar') },
+      { path: '/typography', render: require('./typography') },
       {
-        path: 'other',
+        path: '/other',
         abstract: true,
-        childs: [{ path: 'data-table', render: require('./other/data-table') }]
+        childs: [{ path: '/data-table', render: require('./other/data-table') }]
       }
     ]
   }
 );
 
 app.start();
-routerLinkCheck(app.router, app.options.viewSelector);
+routerLinkCheck(app['router'], app.options.viewSelector);
--- webpack.config.js
+++ webpack.config.js
@@ -1,59 +1,82 @@
 'use strict';
 
-const path = require("path");
-const webpack = require("webpack");
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const path = require('path');
+const webpack = require('webpack');
+var nodeExternals = require('webpack-node-externals');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
+  .BundleAnalyzerPlugin;
 
 const isProd = process.argv.indexOf('-p') !== -1;
 
+/**
+ * 
+ * surplus-material: ./components/index.ts,
+ * app: 
+ */
+
 const webpackConfig = {
-	entry: isProd ? './components/index.ts' : './demo/app.ts',
-	output: {
-		filename: isProd ? './dist/surplus-material.js' : 'app.js'
-	},
-	resolve: {
-		extensions: ['.ts', '.tsx', '.js'],
-		alias: {
-			's-js': path.resolve(path.join(__dirname, 'node_modules', 's-js')) //https://github.com/webpack/webpack/issues/2134#issuecomment-192579511
-		}
-	},
-	devtool: "sourcemap",
-	module: {
-		rules: [
-			{
-				test: /\.tsx?$/, use: [
-					'surplus-loader',
-					'awesome-typescript-loader'
-				]
-			}
-		]
-	},
-	plugins: [
-		new webpack.HotModuleReplacementPlugin()
-	],
-	devServer: {
-		port: 3020,
-		open: true,
-		hot: true,
-		inline: true,
-		openPage: 'demo',
-		historyApiFallback: {
-			index: "./demo/index.html"
-		}
-	}
+  entry: isProd
+    ? {
+        index: './components/index.ts'
+      }
+    : { app: './demo/app.ts' },
+  output: {
+    filename: isProd ? 'dist/[name].js' : '[name].js'
+  },
+  target: isProd ? 'node' : undefined,
+  externals: isProd ? nodeExternals() : undefined,
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+    alias: {
+      's-js': path.resolve(path.join(__dirname, 'node_modules', 's-js')) //https://github.com/webpack/webpack/issues/2134#issuecomment-192579511
+    }
+  },
+  devtool: 'sourcemap',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: ['surplus-loader', 'awesome-typescript-loader']
+      }
+    ]
+  },
+  plugins: isProd
+    ? [
+        new webpack.ContextReplacementPlugin(
+          /moment[\\\/]locale$/,
+          /^\.\/(tr)$/
+        )
+      ]
+    : [
+        new webpack.ContextReplacementPlugin(
+          /moment[\\\/]locale$/,
+          /^\.\/(tr)$/
+        ),
+        new webpack.HotModuleReplacementPlugin()
+      ],
+  devServer: {
+    port: 3020,
+    open: true,
+    hot: true,
+    inline: true,
+    openPage: 'demo',
+    historyApiFallback: {
+      index: './demo/index.html'
+    }
+  }
 };
 
 if (isProd) {
-	webpackConfig.plugins.push(
-		new BundleAnalyzerPlugin({
-			analyzerMode: 'server',
-			analyzerHost: '127.0.0.1',
-			analyzerPort: 8889,
-			reportFilename: 'report.html',
-			openAnalyzer: true,
-			logLevel: 'info'
-		})
-	)
+  webpackConfig.plugins.push(
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'server',
+      analyzerHost: '127.0.0.1',
+      analyzerPort: 8889,
+      reportFilename: 'report.html',
+      openAnalyzer: true,
+      logLevel: 'info'
+    })
+  );
 }
 
-module.exports = webpackConfig
+module.exports = webpackConfig;
